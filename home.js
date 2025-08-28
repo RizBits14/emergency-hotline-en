959fdd6f852ef6f5cf7a2846d211cde14add4482
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-08-28 14:05:52 +0600

    made the coin with DOM

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+// Card Hearts
+
 document.getElementById('card-heart1').addEventListener('click', function(e){ 
     e.preventDefault()
     const navbarHeart = document.getElementById('navbar-heart')
@@ -69,4 +71,105 @@ document.getElementById('card-heart9').addEventListener('click', function(e){
     counting ++ 
     navbarHeart.innerText = counting
     
+})
+
+// Card Call
+document.getElementById('card-call1').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
+})
+document.getElementById('card-call2').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
+})
+document.getElementById('card-call3').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
+})
+document.getElementById('card-call4').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
+})
+document.getElementById('card-call5').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
+})
+document.getElementById('card-call6').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
+})
+document.getElementById('card-call7').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
+})
+document.getElementById('card-call8').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
+})
+document.getElementById('card-call9').addEventListener('click', function() {
+    const navbarCoin = document.getElementById('navbar-coin')
+    let negCount = parseInt(navbarCoin.innerText)
+    if(navbarCoin.innerText > 20){
+        negCount = negCount - 20
+        navbarCoin.innerText = negCount
+    }
+    else{
+        alert('Cant make call')
+    } 
 })
