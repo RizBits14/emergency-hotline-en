e1dc3bfc523a1778d093ae6f1fcdd28876ed92b7
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-08-28 13:40:34 +0600

    made the card-heart working count++

---
+++ home.js
@@ -0,0 +1,72 @@
+document.getElementById('card-heart1').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
+document.getElementById('card-heart2').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
+document.getElementById('card-heart3').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
+document.getElementById('card-heart4').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
+document.getElementById('card-heart5').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
+document.getElementById('card-heart6').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
+document.getElementById('card-heart7').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
+document.getElementById('card-heart8').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
+document.getElementById('card-heart9').addEventListener('click', function(e){ 
+    e.preventDefault()
+    const navbarHeart = document.getElementById('navbar-heart')
+    let counting = parseInt(navbarHeart.innerText)
+    counting ++ 
+    navbarHeart.innerText = counting
+    
+})
